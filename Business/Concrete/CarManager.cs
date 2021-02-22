ebf8d1903bc02ed2ebd689f84b86bed158054169
Ahmet Yoldaş <theahmet4430@gmail.com>
2021-02-22 16:27:17 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        //Business classların içinde sakın newleme yapma.
+        ICarDal _carDal;
+        public CarManager(ICarDal cardal)
+        {
+            _carDal = cardal;
+        }
+
+        public void Add(Car car)
+        {
+            //İş kodları buraya yazılır.
+            //Yetkisi var mı? gibi...
+            _carDal.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            _carDal.Delete(car);
+        }
+
+        public void Update(Car car)
+        {
+            _carDal.Update(car);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _carDal.GetAll();
+        }
+
+        public List<Car> GetById(int BrandId)
+        {
+            return _carDal.GetById(BrandId);
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,62 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace DataAccess.Concrete
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{Id = 1, BrandId = 1, ColorId = 1, ModelYear = "2010"
+                ,DailyPrice = 200000, Description = "Dark Black BMW",  },
+                new Car{Id = 2, BrandId = 2, ColorId = 3, ModelYear = "2012"
+                ,DailyPrice = 500000, Description = "White Mercedes",  },
+                new Car{Id = 3, BrandId = 1, ColorId = 4, ModelYear = "2014"
+                ,DailyPrice = 350000, Description = "Ocean Blue BMW",  },
+                new Car{Id = 4, BrandId = 3, ColorId = 5, ModelYear = "2014"
+                ,DailyPrice = 1000000, Description = "Silver Maserati",  }
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int BrandId)
+        {
+            return _cars.Where(c => c.BrandId == BrandId).ToList();
+        }
+
+        
+    }
+}
